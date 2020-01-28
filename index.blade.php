938347f6d0344e373780b10817a1c7c5aab8e75f
Elif <yildirimelifbasak@gmail.com>
2020-01-28 16:41:47 +0300

    bug fixed

--- index.blade.php
+++ index.blade.php
@@ -1,6 +1,10 @@
 <div id="table">
 
 </div>
+<div id="table2">
+
+</div>
+
 
 <script>
 getRootTable();
@@ -25,6 +29,35 @@ function getRootTable(params) {
     },function(error){
         let json =JSON.parse(error);
         Swal.fire({
+            position: 'center',
+            type :'error',
+            title :json["message"],
+            timer :2000,
+            showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+    console.log($(params).find('#name').text());
+    console.log($(params).find('#date').text());
+    var formData = new FormData();
+    formData.append("paramaters",($(params).find('#name').text()))
+    request("{{API('getTable')}}" ,formData,function(response){
+        Swal.close();
+        $('#table2').html(response);
+        $('#table2').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
         position: 'center',
         type :'error',
         title :json["message"],
@@ -33,14 +66,6 @@ function getRootTable(params) {
         });
 
     });
-}
-
-function getTable(params){
-    
-console.log($(params).find('#name').text());
-console.log($(params).find('#date').text());
-var formData = new FormData();
-formData.append("paramaters",($(params).find('#name').text()))
 }
 
 
@@ -51,6 +76,10 @@ formData.append("paramaters",($(params).find('#name').text()))
 </script><div id="table">
 
 </div>
+<div id="table2">
+
+</div>
+
 
 <script>
 getRootTable();
@@ -75,6 +104,35 @@ function getRootTable(params) {
     },function(error){
         let json =JSON.parse(error);
         Swal.fire({
+            position: 'center',
+            type :'error',
+            title :json["message"],
+            timer :2000,
+            showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+    console.log($(params).find('#name').text());
+    console.log($(params).find('#date').text());
+    var formData = new FormData();
+    formData.append("paramaters",($(params).find('#name').text()))
+    request("{{API('getTable')}}" ,formData,function(response){
+        Swal.close();
+        $('#table2').html(response);
+        $('#table2').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
         position: 'center',
         type :'error',
         title :json["message"],
@@ -83,14 +141,6 @@ function getRootTable(params) {
         });
 
     });
-}
-
-function getTable(params){
-    
-console.log($(params).find('#name').text());
-console.log($(params).find('#date').text());
-var formData = new FormData();
-formData.append("paramaters",($(params).find('#name').text()))
 }
 
 
@@ -101,6 +151,10 @@ formData.append("paramaters",($(params).find('#name').text()))
 </script><div id="table">
 
 </div>
+<div id="table2">
+
+</div>
+
 
 <script>
 getRootTable();
@@ -125,6 +179,35 @@ function getRootTable(params) {
     },function(error){
         let json =JSON.parse(error);
         Swal.fire({
+            position: 'center',
+            type :'error',
+            title :json["message"],
+            timer :2000,
+            showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+    console.log($(params).find('#name').text());
+    console.log($(params).find('#date').text());
+    var formData = new FormData();
+    formData.append("paramaters",($(params).find('#name').text()))
+    request("{{API('getTable')}}" ,formData,function(response){
+        Swal.close();
+        $('#table2').html(response);
+        $('#table2').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
         position: 'center',
         type :'error',
         title :json["message"],
@@ -135,12 +218,154 @@ function getRootTable(params) {
     });
 }
 
+
+
+
+
+
+</script><div id="table">
+
+</div>
+<div id="table2">
+
+</div>
+
+
+<script>
+getRootTable();
+function getRootTable(params) {
+    Swal.fire({
+        position: 'center',
+        type :'info',
+        title :'{{__("Okunuyor...")}}',
+        showConfirmButton:false,
+
+    });
+    request("{{API('getRootTable')}}" ,new FormData(),function(response){
+        Swal.close();
+        $('#table').html(response);
+        $('#table').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+            position: 'center',
+            type :'error',
+            title :json["message"],
+            timer :2000,
+            showConfirmButton:false,
+        });
+
+    });
+}
+
 function getTable(params){
     
-console.log($(params).find('#name').text());
-console.log($(params).find('#date').text());
-var formData = new FormData();
-formData.append("paramaters",($(params).find('#name').text()))
+    console.log($(params).find('#name').text());
+    console.log($(params).find('#date').text());
+    var formData = new FormData();
+    formData.append("paramaters",($(params).find('#name').text()))
+    request("{{API('getTable')}}" ,formData,function(response){
+        Swal.close();
+        $('#table2').html(response);
+        $('#table2').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+        position: 'center',
+        type :'error',
+        title :json["message"],
+      timer :2000,
+      showConfirmButton:false,
+        });
+
+    });
+}
+
+
+
+
+
+
+</script><div id="table">
+
+</div>
+<div id="table2">
+
+</div>
+
+
+<script>
+getRootTable();
+function getRootTable(params) {
+    Swal.fire({
+        position: 'center',
+        type :'info',
+        title :'{{__("Okunuyor...")}}',
+        showConfirmButton:false,
+
+    });
+    request("{{API('getRootTable')}}" ,new FormData(),function(response){
+        Swal.close();
+        $('#table').html(response);
+        $('#table').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+            position: 'center',
+            type :'error',
+            title :json["message"],
+            timer :2000,
+            showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+    console.log($(params).find('#name').text());
+    console.log($(params).find('#date').text());
+    var formData = new FormData();
+    formData.append("paramaters",($(params).find('#name').text()))
+    request("{{API('getTable')}}" ,formData,function(response){
+        Swal.close();
+        $('#table2').html(response);
+        $('#table2').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+        position: 'center',
+        type :'error',
+        title :json["message"],
+      timer :2000,
+      showConfirmButton:false,
+        });
+
+    });
 }
 
 
