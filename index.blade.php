9735faf9e73a123c25a129fdc5d322ccdeff5ecb
Elif <yildirimelifbasak@gmail.com>
2020-01-28 16:25:43 +0300

    bug fixed

--- index.blade.php
+++ index.blade.php
@@ -1,62 +1,151 @@
-<?php
+<div id="table">
 
-function getRootTable(){
+</div>
 
+<script>
+getRootTable();
+function getRootTable(params) {
+    Swal.fire({
+        position: 'center',
+        type :'info',
+        title :'{{__("Okunuyor...")}}',
+        showConfirmButton:false,
 
-    $output=trim(runCommand('ls -d -la /*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
-$array =[];
-foreach(explode("\n",$output)as $line){
-    $fetch =explode('-',$line);
-    $array[]=[
-        "date"=> $fetch[0],
-        "name"=> $fetch[1]
-    ];
-}
-    return view('table',[
-            "value"=>$array,
-           
-            "title"=>[
-                "Dosya Adı", "Degistirme Tarihi"
-            ],
+    });
+    request("{{API('getRootTable')}}" ,new FormData(),function(response){
+        Swal.close();
+        $('#table').html(response);
+        $('#table').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
 
-            "display"=>[
-                "name","date"
-            ],
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+        position: 'center',
+        type :'error',
+        title :json["message"],
+      timer :2000,
+      showConfirmButton:false,
+        });
 
-            "onclick" => "getDetail"
-
-
-    ]);
-        
-}
-function getTable(){
-
-
-    $output=trim(runCommand('ls -d -la  | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
-$array =[];
-foreach(explode("\n",$output)as $line){
-    $fetch =explode('-',$line);
-    $array[]=[
-        "date"=> $fetch[0],
-        "name"=> $fetch[1]
-    ];
-}
-    return view('table',[
-            "value"=>$array,
-           
-            "title"=>[
-                "Dosya Adı", "Degistirme Tarihi"
-            ],
-
-            "display"=>[
-                "name","date"
-            ],
-
-            "onclick" => "getDetail"
-
-
-    ]);
-        
+    });
 }
 
-?>
+function getTable(params){
+    
+console.log($(params).find('#name').text());
+console.log($(params).find('#date').text());
+var formData = new FormData();
+formData.append("paramaters",($(params).find('#name').text()))
+}
+
+
+
+
+
+
+</script><div id="table">
+
+</div>
+
+<script>
+getRootTable();
+function getRootTable(params) {
+    Swal.fire({
+        position: 'center',
+        type :'info',
+        title :'{{__("Okunuyor...")}}',
+        showConfirmButton:false,
+
+    });
+    request("{{API('getRootTable')}}" ,new FormData(),function(response){
+        Swal.close();
+        $('#table').html(response);
+        $('#table').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+        position: 'center',
+        type :'error',
+        title :json["message"],
+      timer :2000,
+      showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+console.log($(params).find('#name').text());
+console.log($(params).find('#date').text());
+var formData = new FormData();
+formData.append("paramaters",($(params).find('#name').text()))
+}
+
+
+
+
+
+
+</script><div id="table">
+
+</div>
+
+<script>
+getRootTable();
+function getRootTable(params) {
+    Swal.fire({
+        position: 'center',
+        type :'info',
+        title :'{{__("Okunuyor...")}}',
+        showConfirmButton:false,
+
+    });
+    request("{{API('getRootTable')}}" ,new FormData(),function(response){
+        Swal.close();
+        $('#table').html(response);
+        $('#table').find("table").DataTable({
+            bFilter:true,
+            "language" :{
+                url:"/turkce.json"
+            }
+        });
+
+    },function(error){
+        let json =JSON.parse(error);
+        Swal.fire({
+        position: 'center',
+        type :'error',
+        title :json["message"],
+      timer :2000,
+      showConfirmButton:false,
+        });
+
+    });
+}
+
+function getTable(params){
+    
+console.log($(params).find('#name').text());
+console.log($(params).find('#date').text());
+var formData = new FormData();
+formData.append("paramaters",($(params).find('#name').text()))
+}
+
+
+
+
+
+
+</script>
