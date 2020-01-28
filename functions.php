e7e3ae47316fd2528aa914082e192701f7924548
Elif <yildirimelifbasak@gmail.com>
2020-01-28 16:41:07 +0300

    bug fixed

--- functions.php
+++ functions.php
@@ -59,4 +59,65 @@ foreach(explode("\n",$output)as $line){
         
 }
 
+?><?php
+
+function getRootTable(){
+
+
+    $output=trim(runCommand('ls -d -la /*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
+$array =[];
+foreach(explode("\n",$output)as $line){
+    $fetch =explode('-',$line);
+    $array[]=[
+        "date"=> $fetch[0],
+        "name"=> $fetch[1]
+    ];
+}
+    return view('table',[
+            "value"=>$array,
+           
+            "title"=>[
+                "Dosya Adı", "Degistirme Tarihi"
+            ],
+
+            "display"=>[
+                "name","date"
+            ],
+
+            "onclick" => "getTable"
+
+
+    ]);
+        
+}
+function getTable(){
+    $param_value = request("paramaters");
+
+    $output=trim(runCommand('ls -d -la '.$param_value .'*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
+$array =[];
+foreach(explode("\n",$output)as $line){
+    $fetch =explode('-',$line);
+    $array[]=[
+        "date"=> $fetch[0],
+        "name"=> $fetch[1]
+    ];
+}
+    return view('table',[
+            "value"=>$array,
+           
+            "title"=>[
+                "Dosya Adı", "Degistirme Tarihi"
+            ],
+
+            "display"=>[
+                "name","date"
+            ],
+
+            "onclick" => "getTable"
+
+
+    ]);
+        
+}
+
 ?>
