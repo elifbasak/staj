95f56684a5ec4c86e83602c288ff6c092f9066e6
Elif <yildirimelifbasak@gmail.com>
2020-01-29 16:35:51 +0300

    klasör ekleme görevi

--- index.blade.php
+++ index.blade.php
@@ -1,312 +1,55 @@
-<div id="table">
 
+<div class="container">
+        <div class="row ">
+
+         
+        <div class="col-md-3">
+        <input type="text" id="myInput">
+
+<button type="button" id="myBtn">klasor ekle</button>
 </div>
-<div id="table2">
 
-</div>
-
-
-<script>
-getRootTable();
-function getRootTable(params) {
-    Swal.fire({
-        position: 'center',
-        type :'info',
-        title :'{{__("Okunuyor...")}}',
-        showConfirmButton:false,
-
-    });
-    request("{{API('getRootTable')}}" ,new FormData(),function(response){
-        Swal.close();
-        $('#table').html(response);
-        $('#table').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-            position: 'center',
-            type :'error',
-            title :json["message"],
-            timer :2000,
-            showConfirmButton:false,
-        });
-
-    });
-}
-
-function getTable(params){
     
-    console.log($(params).find('#name').text());
-    console.log($(params).find('#date').text());
-    var formData = new FormData();
-    formData.append("paramaters",($(params).find('#name').text()))
-    request("{{API('getTable')}}" ,formData,function(response){
-        Swal.close();
-        $('#table2').html(response);
-        $('#table2').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-        position: 'center',
-        type :'error',
-        title :json["message"],
-      timer :2000,
-      showConfirmButton:false,
-        });
-
-    });
-}
-
-
-
-
-
-
-</script><div id="table">
-
+        <div class="col-md-5">
+        <div id="table">
 </div>
+        </div>
+
+
+
+
+
+<div class="col-md-4">
 <div id="table2">
+</div>
 
 </div>
+
+
+
+</div>
+</div>
+
+
 
 
 <script>
-getRootTable();
-function getRootTable(params) {
-    Swal.fire({
-        position: 'center',
-        type :'info',
-        title :'{{__("Okunuyor...")}}',
-        showConfirmButton:false,
+$("#myBtn").click(function(){
 
-    });
-    request("{{API('getRootTable')}}" ,new FormData(),function(response){
-        Swal.close();
-        $('#table').html(response);
-        $('#table').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
+    var str = $("#myInput").val();
+   
 
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-            position: 'center',
-            type :'error',
-            title :json["message"],
-            timer :2000,
-            showConfirmButton:false,
-        });
-
-    });
-}
-
-function getTable(params){
     
-    console.log($(params).find('#name').text());
-    console.log($(params).find('#date').text());
     var formData = new FormData();
-    formData.append("paramaters",($(params).find('#name').text()))
-    request("{{API('getTable')}}" ,formData,function(response){
-        Swal.close();
-        $('#table2').html(response);
-        $('#table2').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-        position: 'center',
-        type :'error',
-        title :json["message"],
-      timer :2000,
-      showConfirmButton:false,
-        });
-
+    formData.append("par",str);
+    request("{{API('klasor')}}" ,formData,function(response){
+        console.log("deneme");
     });
-}
+
+});
+   
 
 
-
-
-
-
-</script><div id="table">
-
-</div>
-<div id="table2">
-
-</div>
-
-
-<script>
-getRootTable();
-function getRootTable(params) {
-    Swal.fire({
-        position: 'center',
-        type :'info',
-        title :'{{__("Okunuyor...")}}',
-        showConfirmButton:false,
-
-    });
-    request("{{API('getRootTable')}}" ,new FormData(),function(response){
-        Swal.close();
-        $('#table').html(response);
-        $('#table').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-            position: 'center',
-            type :'error',
-            title :json["message"],
-            timer :2000,
-            showConfirmButton:false,
-        });
-
-    });
-}
-
-function getTable(params){
-    
-    console.log($(params).find('#name').text());
-    console.log($(params).find('#date').text());
-    var formData = new FormData();
-    formData.append("paramaters",($(params).find('#name').text()))
-    request("{{API('getTable')}}" ,formData,function(response){
-        Swal.close();
-        $('#table2').html(response);
-        $('#table2').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-        position: 'center',
-        type :'error',
-        title :json["message"],
-      timer :2000,
-      showConfirmButton:false,
-        });
-
-    });
-}
-
-
-
-
-
-
-</script><div id="table">
-
-</div>
-<div id="table2">
-
-</div>
-
-
-<script>
-getRootTable();
-function getRootTable(params) {
-    Swal.fire({
-        position: 'center',
-        type :'info',
-        title :'{{__("Okunuyor...")}}',
-        showConfirmButton:false,
-
-    });
-    request("{{API('getRootTable')}}" ,new FormData(),function(response){
-        Swal.close();
-        $('#table').html(response);
-        $('#table').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-            position: 'center',
-            type :'error',
-            title :json["message"],
-            timer :2000,
-            showConfirmButton:false,
-        });
-
-    });
-}
-
-function getTable(params){
-    
-    console.log($(params).find('#name').text());
-    console.log($(params).find('#date').text());
-    var formData = new FormData();
-    formData.append("paramaters",($(params).find('#name').text()))
-    request("{{API('getTable')}}" ,formData,function(response){
-        Swal.close();
-        $('#table2').html(response);
-        $('#table2').find("table").DataTable({
-            bFilter:true,
-            "language" :{
-                url:"/turkce.json"
-            }
-        });
-
-    },function(error){
-        let json =JSON.parse(error);
-        Swal.fire({
-        position: 'center',
-        type :'error',
-        title :json["message"],
-      timer :2000,
-      showConfirmButton:false,
-        });
-
-    });
-}
-
-
-
-
-
-
-</script><div id="table">
-
-</div>
-<div id="table2">
-
-</div>
-
-
-<script>
 getRootTable();
 function getRootTable(params) {
     Swal.fire({
@@ -374,3 +117,5 @@ function getTable(params){
 
 
 </script>
+</body>
+</html>
