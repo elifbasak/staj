8eeec23caf81e89c0a195324317059c277fd1b80
Elif <yildirimelifbasak@gmail.com>
2020-01-31 16:31:05 +0300

    postgresql

--- functions.php
+++ functions.php
@@ -2,127 +2,28 @@
 
 function getRootTable(){
 
+    $output2=trim(runCommand('apt show postgresql-12'));
+   // dd($output2);
+  
+    $array2= explode("\n",$output2);
+   // dd($array2[0]);
+   // dd($array2[1]);
+    $pos = strrpos($array2[1], ":");
+    $pos2=strrpos($array2[1], "Version:");
+    if(trim($pos2)!="0"){
+        return respond("paket yüklü değil",201);
+    }
+    $versiyon=substr($array2[1],$pos+1 );
+   $output= trim(runCommand('dpkg --get-selections | grep -v deinstall | awk \'{print $1} \' |  grep \'^postgresql-12$\''));
+   
+if($output=="postgresql-12")
+    return $versiyon;
+else  return respond("paket yüklü değil",201);
+//dd(($versiyon));
 
-    $output=trim(runCommand('ls -d -la /*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
-$array =[];
-foreach(explode("\n",$output)as $line){
-    $fetch =explode('-',$line);
-    $array[]=[
-        "date"=> $fetch[0],
-        "name"=> $fetch[1]
-    ];
-}
-    return view('table',[
-        "value"=>$array,
-           
-        "title"=>[
-            "Dosya Adı", "Degistirme Tarihi"
-        ],
-
-        "display"=>[
-            "name","date"
-        ],
-        "menu"=> [
-            "Sil"=> [
-                "target"=> "delete_log_archive",
-                "icon"=>"fa-trash"
-            ],
-            "Geri Yükle"=> [
-                "target"=> "restore_log_archive",
-                "icon"=>"fa-window-restore"
-            ]
-            ],
-
-        "onclick" => "getTable"
-
-    ]);
+    
         
 }
-function getTable(){
-    $param_value = request("paramaters");
-
-    $output=trim(runCommand('ls -d -la '.$param_value.'*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
-$array =[];
-foreach(explode("\n",$output)as $line){
-    $fetch =explode('-',$line);
-    $array[]=[
-        "date"=> $fetch[0],
-        "name"=> $fetch[1]
-    ];
-}
-    return view('table',[
-            "value"=>$array,
-           
-            "title"=>[
-                "Dosya Adı", "Degistirme Tarihi"
-            ],
-
-            "display"=>[
-                "name","date"
-            ],
-            "menu"=> [
-                "Sil"=> [
-                    "target"=> "delete_log_archive",
-                    "icon"=>"fa-trash"
-                ],
-                "Geri Yükle"=> [
-                    "target"=> "restore_log_archive",
-                    "icon"=>"fa-window-restore"
-                ]
-                ],
-
-            "onclick" => "getTable"
-
-
-    ]);
-        
-}
-function klasor(){
-    $param_value = request("par");
-    runCommand('mkdir /home/linux/'.$param_value);
-    return respond("Klasör başarıyla eklendi");
-}
-
-function getTableGeriDon(){
-    $param_value=trim(runCommand('cd ..'));
-    $output=trim(runCommand('ls -d -la '.$param_value.'*/ | awk \'{print $6 " " $7 " "$8 "-" $9} \''));
-$array =[];
-foreach(explode("\n",$output)as $line){
-    $fetch =explode('-',$line);
-    $array[]=[
-        "date"=> $fetch[0],
-        "name"=> $fetch[1]
-    ];
-}
-    return view('table',[
-            "value"=>$array,
-           
-            "title"=>[
-                "Dosya Adı", "Degistirme Tarihi"
-            ],
-
-            "display"=>[
-                "name","date"
-            ],
-            "menu"=> [
-                "Sil"=> [
-                    "target"=> "delete_log_archive",
-                    "icon"=>"fa-trash"
-                ],
-                "Geri Yükle"=> [
-                    "target"=> "restore_log_archive",
-                    "icon"=>"fa-window-restore"
-                ]
-                ],
-
-            "onclick" => "getTable"
-
-
-    ]);
-        
-}
-
-
 
 
 ?>
